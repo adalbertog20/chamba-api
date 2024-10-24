ecd9c921fc1c12793a986bcbd9819176b1bfed66
adalbertog20 <adalbertog_20@alu.uabcs.mx>
2024-10-23 19:44:59 -0700

    add new info on DashboardController.php

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Chamba;
+use App\Models\Image;
 use App\Models\RequestChamba;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,9 +14,14 @@ class DashboardController extends Controller
     {
         $chambas = Chamba::with(['user'])->where('worker_id', Auth::id())->count();
         $requests = RequestChamba::with(['worker'])->where('worker_id', Auth::id())->where('status', 'pending')->count();
+        $images = Image::where('user_id', Auth::id())->count();
+        $chambas_realizadas = RequestChamba::where('worker_id', Auth::id())->where('status', 'completed')->count();
+
         return response()->json([
             'chambas' => $chambas,
-            'requests' => $requests
+            'requests' => $requests,
+            'images' => $images,
+            'chambas_realizadas' => $chambas_realizadas,
         ]);
     }
 }
